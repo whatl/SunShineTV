bb022a618f2712fa090abe31a8fe074ab9ec6f34
geng <lsbalabala@gmail.com>
2025-10-02 13:15:21 +0800

    opt config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,14 +43,11 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   const storageType = process.env.NEXT_PUBLIC_STORAGE_TYPE || 'localstorage';
-
+  const notLocalStorage = storageType !== 'localstorage'
+  // 非本地存储从配置读取
   let siteName = process.env.NEXT_PUBLIC_SITE_NAME || 'SunShineTV';
-  let announcement =
-    process.env.ANNOUNCEMENT ||
-    '本网站仅提供影视信息搜索服务，所有内容均来自第三方网站。本站不存储任何视频资源，不对任何内容的准确性、合法性、完整性负责。';
-  let footerText =
-    process.env.FOOTER_TEXT ||
-    '本站所有视频和图片均来自互联网收集而来，版权归原创者所有，本网站只提供web页面服务，并不提供资源存储，也不参与录制、上传。若本站收录的节目无意侵犯了贵司版权，请发邮件至chuichui#gmail.com（#换成@）';
+  let announcement = !notLocalStorage ? process.env.ANNOUNCEMENT || '' : '';
+  let footerText = !notLocalStorage ? process.env.FOOTER_TEXT || '' : '';
   let footerLinks: { name: string; url: string }[] = [];
 
   let doubanProxyType = process.env.NEXT_PUBLIC_DOUBAN_PROXY_TYPE || 'cmliussss-cdn-tencent';
@@ -66,7 +63,7 @@ export default async function RootLayout({
     type: 'movie' | 'tv';
     query: string;
   }[];
-  if (storageType !== 'localstorage') { // 不是本地存储才有
+  if (notLocalStorage) { // 不是本地存储才有
     const config = await getConfig();
     siteName = config.SiteConfig.SiteName;
     announcement = config.SiteConfig.Announcement;
